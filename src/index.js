b977980e04ac32d3c42c66e35abbf880f96dbf1e
veeru78 <prakashmishra6263@gmail.com>
2022-03-10 22:55:11 +0530

    assignment done

--- src/index.js
+++ src/index.js
@@ -8,20 +8,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://prakash:prakash@cluster0.4qxv0.mongodb.net/prakash?authSource=admin&replicaSet=atlas-13sjqg-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
-
-app.use('/', route);
+app.use('/', route)
 
 
 app.listen(process.env.PORT || 3000, function () {
